798236679145f31632b371b3925750e0b6470ac3
yqlbu <yqlbu@bu.edu>
2023-04-05 21:26:25 +0800

    patch: set default theme and content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,47 +1,48 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Daeuniverse',
-  tagline: 'A Linux high-performance transparent proxy solution based on eBPF.',
-  favicon: 'img/favicon.ico',
+  title: "Dae",
+  tagline: "A Linux high-performance transparent proxy solution based on eBPF",
+  // favicon: 'img/favicon.ico',
+  favicon: "https://github.com/daeuniverse/dae/raw/main/logo.png",
 
   // Set the production url of your site here
-  url: 'https://dae.v2raya.org',
+  url: "https://dae.v2raya.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'daeuniverse', // Usually your GitHub org/user name.
-  projectName: 'dae', // Usually your repo name.
+  organizationName: "daeuniverse", // Usually your GitHub org/user name.
+  projectName: "dae", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -51,64 +52,35 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Dae',
+        title: "Dae",
         logo: {
-          alt: 'Logo',
-          src: 'img/logo.svg',
+          alt: "Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentation",
           },
           {
-            href: 'https://github.com/daeuniverse/dae',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/daeuniverse/dae",
+            label: "GitHub",
+            position: "right",
           },
           {
-            href: 'https://t.me/daeuniverse',
-            label: 'Telegram',
-            position: 'right',
+            href: "https://t.me/daeuniverse",
+            label: "Telegram",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Telegram',
-                href: 'https://t.me/daeuniverse',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/daeuniverse/dae',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} @daeuniverse. Built with Docusaurus.`,
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} @daeuniverse. Built with Docusaurus, hosted on Kubernetes.`,
       },
       prism: {
         theme: lightCodeTheme,
